314905400fdb913aaf2f8588928e6ae5376c3f05
Gabriel Ferraz <135299312+gabrielferrazz@users.noreply.github.com>
2023-09-19 09:06:59 -0300

    upload.

---
+++ Projeto Rocketseat/upload-ai-web/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+export function App() {
+  <div>
+    <div>
+      <h1>Upload.ai</h1>
+
+      <div>
+        <span>Desenvolvido com 💜 no NLW da Rockeatseat</span>
+        <Button>Github</Button>
+      </div>
+    </div>
+  </div>
+}
