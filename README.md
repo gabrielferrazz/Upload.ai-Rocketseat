67ff16316e0d0a58d252d9c1b58c17c5ed69f1bb
gabrielferrazz <gabrielrferraz@hotmail.com>
2023-09-20 00:31:42 -0300

    upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Upload.ai-Rocketseat
+
+1 - instale o Node.
+
+2- Rode o comando npm install -g pnpm
+
+3- 
--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -1,14 +1,16 @@
 import { Button } from "./components/ui/button"
 
 export function App() {
-  <div>
+  return (    
     <div>
-      <h1>Upload.ai</h1>
-
       <div>
-        <span>Desenvolvido com 💜 no NLW da Rockeatseat</span>
-        <Button>Github</Button>
+        <h1>Upload.ai</h1>
+
+        <div>
+          <span>Desenvolvido com 💜 no NLW da Rocketseat</span>
+          <Button>Github</Button>
+        </div>
       </div>
     </div>
-  </div>
+  )
 }
